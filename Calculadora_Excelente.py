ce754e27cfe7d91aad0b6c81004ba5a8d658b801
Gustavo Rodrigos <Gustavo Rodrigos@DESKTOP-BLHKUR4>
2021-08-26 20:51:03 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora_Excelente.py
@@ -0,0 +1,26 @@
+#Wesley Almeida 26/08/2021 Calculadora Excelente
+
+print("Bem vindo a calculadora Excelente")
+
+a = int(input("Digite o primeiro valor: "))
+
+b = int(input("Digite o segundo valor: "))
+
+soma = a + b
+
+subtracao = a - b 
+
+multiplicacao = a * b 
+
+divisao = a / b 
+
+print()
+
+print("O resultado total é: ",soma)
+
+print("Resultado da subtracao é: ",subtracao)
+
+print("Resultado da multiplicacao é: ",multiplicacao)
+
+print("Resultado da divisao é: ",divisao)
+
